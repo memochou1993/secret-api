de4da9450289bc9d2b1ddcbbcf7037c44e065f2d
Memo Chou <memochou1993@gmail.com>
2022-04-03 15:14:18 +0800

    Add update user api

--- handler/user.go
+++ handler/user.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"errors"
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/memochou1993/secret-api/database"
 	"github.com/memochou1993/secret-api/util"
@@ -30,4 +31,31 @@ func CreateUser(c echo.Context) error {
 	data.Password = hash
 	database.DB().Create(data)
 	return c.JSON(http.StatusCreated, nil)
+}
+
+func UpdateUser(c echo.Context) error {
+	userID := c.Get("user").(*jwt.Token).Claims.(*TokenClaims).UserID
+	data := &database.User{}
+	if err := c.Bind(data); err != nil {
+		return err
+	}
+	if err := c.Validate(data); err != nil {
+		return err
+	}
+	user := &database.User{}
+	tx := database.DB().Where(&database.User{Email: data.Email}).First(user)
+	if !errors.Is(tx.Error, gorm.ErrRecordNotFound) && user.ID != userID {
+		return c.JSON(http.StatusBadRequest, echo.Map{
+			"message": "The email has already been taken",
+		})
+	}
+	if data.Password != "" {
+		hash, err := util.HashPassword(data.Password)
+		if err != nil {
+			return err
+		}
+		data.Password = hash
+	}
+	database.DB().Where(&database.User{ID: userID}).Updates(data)
+	return c.JSON(http.StatusOK, nil)
 }
